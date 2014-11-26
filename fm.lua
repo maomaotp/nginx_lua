02a0f546ae46bd17b20eb13a168fb84e51a0d889
maomaotp <404709954@qq.com>
2014-11-26 16:18:03 +0800

    负载均衡

--- fm.lua
+++ fm.lua
@@ -5,23 +5,27 @@ events {
 	worker_connections 1024;
 }
 http {
-	upstream backend{
-		server 127.0.0.1:8080;
-		server 123.57.41.242:8080;
+	keepalive_timeout 120;
+	upstream fmserver{
+		server 127.0.0.1:8080 weight=2;
+		server 123.57.41.242:8080 weight=2;
 	}
 
 	#default_type application/octet-stream;
 	default_type application/json;
 	server {
 		listen 8080;
-#server_name "http://192.168.1.120:8080";
+		server_name localhost;
         location /query_top{
+			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/redis.lua";
 		}
         location /lingbanfm{
+			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/mysql.lua";
 		}
 		location /test{
+			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/json.lua";
 		}
     }
