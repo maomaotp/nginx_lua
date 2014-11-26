919327a4027871a47416bd42d456fb5036f97611
maomaotp <404709954@qq.com>
2014-11-26 16:47:07 +0800

    fmserver

--- fm.lua
+++ fm.lua
@@ -7,25 +7,21 @@ events {
 http {
 	keepalive_timeout 120;
 	upstream fmserver{
-		server 127.0.0.1:8080 weight=2;
-		server 123.57.41.242:8080 weight=2;
+		server 192.168.1.120:8080 weight=2;
+		server 192.168.1.120:8090 weight=2;
 	}
 
 	#default_type application/octet-stream;
 	default_type application/json;
 	server {
 		listen 8080;
-		server_name localhost;
         location /query_top{
-			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/redis.lua";
 		}
         location /lingbanfm{
-			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/mysql.lua";
 		}
 		location /test{
-			proxy_pass http://fmserver;
 			content_by_lua_file "nginx_lua/json.lua";
 		}
     }
