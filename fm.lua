887f40dc0eef07adf79f1515023e55e045d37e24
maomaotp <404709954@qq.com>
2014-11-27 13:13:02 +0800

    增加日志模块

--- fm.lua
+++ fm.lua
@@ -1,18 +1,23 @@
-worker_processes  1;
-error_log logs/error.log;
+worker_processes  2;
+error_log logs/error.log error;
+
 events {
 	use epoll;
-	worker_connections 1024;
+	worker_connections 2048;
 }
 http {
+	default_type application/json;
+
+	log_format main '[$time_local] $remote_addr $request_uri '
+					'[$http_user_agent] $bytes_sent $request_time '
+					'"$request_body" $host $status';
+	access_log logs/access.log main;
+
 	keepalive_timeout 120;
 	upstream fmserver{
 		server 192.168.1.120:8080 weight=2;
 		server 192.168.1.120:8090 weight=2;
 	}
-
-	#default_type application/octet-stream;
-	default_type application/json;
 	server {
 		listen 8080;
         location /query_top{
